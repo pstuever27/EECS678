e85c5594c018ace3ba6a5ec8d9318d4176b568f8
root <root@Pauls-Desktop>
2023-02-09 16:33:01 -0600

    lab 2

--- eecs678-lab2-signals/signals.c
+++ eecs678-lab2-signals/signals.c
@@ -21,6 +21,8 @@ void catch_int(int sig_num)
      * exit or not */
     printf("\nReally exit? [Y/n]: ");
     fflush(stdout);
+    //add timer for 5 seconds
+    alarm( 5 );
     fgets(answer, sizeof(answer), stdin);
     if (answer[0] == 'n' || answer[0] == 'N') {
       printf("\nContinuing\n");
@@ -29,6 +31,8 @@ void catch_int(int sig_num)
        * Reset Ctrl-C counter
        */
       ctrl_c_count = 0;
+      //reflect that we got a response so that we don't accidentally exit while the alarm is finishing
+      got_response = 1;
     }
     else {
       printf("\nExiting...\n");
@@ -51,9 +55,21 @@ void catch_tstp(int sig_num)
 /* If the user DOES NOT RESPOND before the alarm time elapses, the program should exit */
 /* If the user RESPONDEDS before the alarm time elapses, the alarm should be cancelled */
 //YOUR CODE
-unsigned int alarm( unsigned int seconds )
+void catch_alarm( int sig_num )
 {
-  //SIGALARM after a number of seconds
+  if( got_response != 0)
+  {
+    got_response = 0;
+  }
+  else
+  {
+    //SIGALARM after a number of seconds
+    printf("\n\nUser taking too long to respond. Exiting...\n\n");
+    fflush(stdout);
+    exit(0);
+  }
+
+
 }
 
 int main(int argc, char* argv[])
@@ -65,6 +81,7 @@ int main(int argc, char* argv[])
   /* type "man memset" on the terminal and take reference from it */
   /* if the sa memory location is reset this way, then no garbage value can create undefined behavior with the signal handlers */
   //YOUR CODE
+  memset( &sa, 0, sizeof( sa ) );
 
 
   sigset_t mask_set;  /* used to set a signal masking set. */
@@ -73,21 +90,48 @@ int main(int argc, char* argv[])
   /* STEP - 3 (10 points) */
   /* setup mask_set - fill up the mask_set with all the signals to block*/
   //YOUR CODE
+
+  sigfillset( &mask_set );
   
   
   /* STEP - 4 (10 points) */
   /* ensure in the mask_set that the alarm signal does not get blocked while in another signal handler */
   //YOUR CODE
+
+  sigdelset( &mask_set, 14 );
   
   
   /* STEP - 5 (20 points) */
   /* set signal handlers for SIGINT, SIGTSTP and SIGALRM */
   //YOUR CODE
+
+  //SIGINT handlers
+  //SIGINT = 2
+  sa.sa_mask = mask_set;
+  sa.sa_handler = &catch_int;
+  sigaction( 2, &sa, NULL );
+
+  //SIGTSTP handlers
+  //SIGTSTP = 18, 20, 24
+  sa.sa_handler = &catch_tstp;
+  sigaction( 18, &sa, NULL );
+  sigaction( 20, &sa, NULL );
+  sigaction( 24, &sa, NULL );
+
+  //SIGALARM handlers
+  //SIGALARM = 14
+  sa.sa_handler = &catch_alarm;
+  sigaction( 14, &sa, NULL );
+
   
   
   /* STEP - 6 (10 points) */
   /* ensure that the program keeps running to receive the signals */
   //YOUR CODE
+  while( 1 )
+  {
+    pause();
+  }
   
 
   return 0;
